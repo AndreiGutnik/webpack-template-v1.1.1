f282415952ea8f810a25a5127bc5ed4d59b45aaa
AndreiGutnik <andreigutnik@gmail.com>
2024-05-29 20:03:07 +0000

    Deploying to gh-pages from @ AndreiGutnik/webpack-template-v1.1.1@e2da84f2f4809c4483c56ac913f803ed08f68739 🚀

---
+++ 652.22b5315106fc68a5512f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebpack_react_ts_template=self.webpackChunkwebpack_react_ts_template||[]).push([[652],{652:(e,t,a)=>{a.r(t),a.d(t,{default:()=>l});var c=a(976);function l(){return React.createElement("div",null,React.createElement("h1",null,"MainPage"),React.createElement(c.N_,{to:"/product"},"Product"))}}}]);
+//# sourceMappingURL=652.22b5315106fc68a5512f.js.map
